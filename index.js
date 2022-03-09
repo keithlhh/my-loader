adb73654975c0b39c1360d30bc1d5213b1898a9e
李洪辉 <yishui.lhh@raycloud.com>
2022-03-10 00:09:34 +0800

    build

--- index.js
+++ index.js
@@ -2,5 +2,9 @@
 	MIT License http://www.opensource.org/licenses/mit-license.php
 	Author Tobias Koppers @sokra
 */
-console.log(require("./src/index.js"), '8888888')
-module.exports = require("./src/index.js").loader;
+const aaa = require("./src/index.js");
+console.log(require(aaa), '8888888')
+module.exports = (source) => {
+	console.log(source ,' jjjjjjjjjjj')
+	return source
+};
